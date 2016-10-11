5ad4424878ffa105df6d8cf3ecf19ec6f26a8de3
Ram Alapure <alapureram@gmail.com>
2016-10-11 13:11:31 +0530

    Weather Observation Station 6

---
+++ Basic-Select/WeatherObservationStation6.sql
@@ -0,0 +1,9 @@
+/*
+Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Enter your query here.
+*/
+
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
+
+
